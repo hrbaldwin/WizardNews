8099f6208059005443953b9dd017b5407bc59e24
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-10-24 14:27:44 -0500

    monday- end of day commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const postBank = require("./postBank");
+
+const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+    <ul>
+      ${posts.map((post) => `<li>${postBank}</li>`)}
+    </ul>
+    </body>
+  </html>`;
+});
 
 const PORT = 1337;
 
